74178d41c2cc8bb2df0dc78729b4e230379ba0d1
Michael Kuzmin <mkuzmin@gmail.com>
2017-08-18 02:24:59 +0300

    verity vm name

--- builder_acc_test.go
+++ builder_acc_test.go
@@ -3,12 +3,15 @@ package main
 import (
 	"testing"
 	builderT "github.com/hashicorp/packer/helper/builder/testing"
+	"fmt"
+	"github.com/hashicorp/packer/packer"
 )
 
 func TestBuilderAcc_basic(t *testing.T) {
 	builderT.Test(t, builderT.TestCase{
 		Builder:  &Builder{},
 		Template: testBuilderAccBasic,
+		Check: checkBasic(),
 	})
 }
 
@@ -31,3 +34,43 @@ const testBuilderAccBasic = `
 	}]
 }
 `
+
+func checkBasic() builderT.TestCheckFunc {
+	return func(artifacts []packer.Artifact) error {
+		if len(artifacts) > 1 {
+			return fmt.Errorf("more than 1 artifact")
+		}
+
+		artifactRaw := artifacts[0]
+		artifact, ok := artifactRaw.(*Artifact)
+		if !ok {
+			return fmt.Errorf("unknown artifact: %#v", artifactRaw)
+		}
+
+		conn, err := testConn()
+		if err != nil {
+			return err
+		}
+
+		vm, err := conn.finder.VirtualMachine(conn.ctx, artifact.Name)
+		if err != nil {
+			return err
+		}
+
+		if vm.Name() != artifact.Name {
+			return fmt.Errorf("Invalid VM name: expected '%v', got '%v'", artifact.Name, vm.Name())
+		}
+
+		return nil
+	}
+}
+func testConn() (*Driver, error) {
+	config := &ConnectConfig{
+		VCenterServer: "vcenter.vsphere55.test",
+		Username: "root",
+		Password: "jetbrains",
+		InsecureConnection: true,
+	}
+
+	return NewDriver(config)
+}
