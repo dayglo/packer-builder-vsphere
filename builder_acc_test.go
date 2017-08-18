e1c8cf40ae5858644f0a9ea51e8b8302925541b2
Michael Kuzmin <mkuzmin@gmail.com>
2017-08-18 07:50:20 +0300

    fix test

--- builder_acc_test.go
+++ builder_acc_test.go
@@ -40,7 +40,7 @@ func TestBuilderAcc_default(t *testing.T) {
 	builderT.Test(t, builderT.TestCase{
 		Builder:  &Builder{},
 		Template: renderConfig(defaultConfig()),
-		Check:    checkDefault(),
+		Check:    checkDefault(defaultConfig()),
 	})
 }
 
@@ -60,7 +60,7 @@ func defaultConfig() map[string]interface{} {
 	}
 }
 
-func checkDefault() builderT.TestCheckFunc {
+func checkDefault(config map[string]interface{}) builderT.TestCheckFunc {
 	return func(artifacts []packer.Artifact) error {
 		if len(artifacts) > 1 {
 			return fmt.Errorf("more than 1 artifact")
@@ -82,45 +82,37 @@ func checkDefault() builderT.TestCheckFunc {
 			return err
 		}
 
-		var v mo.VirtualMachine
-		err = vm.Properties(conn.ctx, vm.Reference(), []string{"layoutEx.disk"}, &v)
+		var vmInfo mo.VirtualMachine
+		err = vm.Properties(conn.ctx, vm.Reference(), []string{"name", "runtime.host", "resourcePool", "layoutEx.disk"}, &vmInfo)
 		if err != nil {
 			return err
 		}
 
-		if vm.Name() != artifact.Name {
-			return fmt.Errorf("Invalid VM name: expected '%v', got '%v'", artifact.Name, vm.Name())
+		if vmInfo.Name != config["vm_name"] {
+			return fmt.Errorf("Invalid VM name: expected '%v', got '%v'", config["vm_name"], vmInfo.Name)
 		}
 
-		host, err := vm.HostSystem(conn.ctx)
-		if err != nil {
-			return err
-		}
-		var h mo.HostSystem
-		err = host.Properties(conn.ctx, host.Reference(), []string{"name"}, &h)
+		var hostInfo mo.HostSystem
+		err = vm.Properties(conn.ctx, vmInfo.Runtime.Host.Reference(), []string{"name"}, &hostInfo)
 		if err != nil {
 			return err
 		}
 
-		hostname := "esxi-1.vsphere55.test"
-		if h.Name != hostname {
-			return fmt.Errorf("Invalid host name: expected '%v', got '%v'", hostname, h.Name)
+		if hostInfo.Name != config["host"] {
+			return fmt.Errorf("Invalid host name: expected '%v', got '%v'", config["host"], hostInfo.Name)
 		}
 
-		pool, err := vm.ResourcePool(conn.ctx)
+		var rpInfo = mo.ResourcePool{}
+		err = vm.Properties(conn.ctx, vmInfo.ResourcePool.Reference(), []string{"owner", "parent"}, &rpInfo)
 		if err != nil {
 			return err
 		}
-		var p mo.ResourcePool
-		err = pool.Properties(conn.ctx, pool.Reference(), []string{"owner", "parent"}, &p)
-		if err != nil {
-			return err
-		}
-		if p.Owner != *p.Parent {
+
+		if rpInfo.Owner != *rpInfo.Parent {
 			return fmt.Errorf("Not a root resource pool")
 		}
 
-		if len(v.LayoutEx.Disk[0].Chain) != 1 {
+		if len(vmInfo.LayoutEx.Disk[0].Chain) != 1 {
 			return fmt.Errorf("Not a full clone")
 		}
 
